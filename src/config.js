4a997f0b26dcd0f504863cf63a8332fb9b2060b3
MattGirolami <MattGirolami@users.noreply.github.com>
2019-05-28 21:19:32 -0600

    Add basic styling, header, 404, and nav bar to app. include aws amplify, react router

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-upload-mtg"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://apz1q6axx3.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_5O500gPJW",
+      APP_CLIENT_ID: "7ha2683vcjr82r0o8so37gj5fn",
+      IDENTITY_POOL_ID: "us-east-1:27298cd3-68ec-4f9f-93e5-2eaa80abab69"
+    }
+  };
+  
